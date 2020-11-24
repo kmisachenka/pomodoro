cda5f1563661ff2d1009a02ea03f84a6160665fe
Kiryl Misachenka <kmisachenka@gmail.com>
2020-11-24 10:48:19 +0300

    chore(deps): update deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/core';
+import { Button } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import React from 'react';
 
--- src/pages/second-page.tsx
+++ src/pages/second-page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/core';
+import { Button } from '@chakra-ui/react';
 import React from 'react';
 
 import { Link, useTranslation } from '../i18n';
